d4a9ef45b35beffd49f94c395e3f8f435b14945f
Mike <mclancy96@gmail.com>
2025-05-21 12:53:08 -0400

    Update existing functions to use this

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
-/* Your Code Here */
+function createEmployeeRecord(employeeInfo) {
+	return {
+		firstName: employeeInfo[0],
+		familyName: employeeInfo[1],
+		title: employeeInfo[2],
+		payPerHour: employeeInfo[3],
+		timeInEvents: [],
+		timeOutEvents: []
+	}
+}
+
+function createEmployeeRecords(employeeInfoArray) {
+	return employeeInfoArray.map(employeeInfo => createEmployeeRecord(employeeInfo))
+}
+
+function createTimeInEvent(timePunch) {
+	this.timeInEvents.push(parseTimePunch(timePunch, "TimeIn"))
+	return this
+}
+
+function createTimeOutEvent(timePunch) {
+	this.timeOutEvents.push(parseTimePunch(timePunch, "TimeOut"))
+	return this
+}
+
+function parseTimePunch(timePunchString, type) {
+	const timePunchFormat = /^\d{4}-\d{2}-\d{2} \d{4}$/;
+	if (!timePunchFormat.test(timePunchString)) {
+		throw new Error("Time punch format must be 'YYYY-MM-DD HHMM'");
+	}
+	return {
+		type: type,
+		hour: Number.parseInt(timePunchString.split(' ')[1]),
+		date: timePunchString.split(' ')[0]
+	}
+}
+
+function getDuration(timeInTime, timeOutTime) {
+	const timeInHour = Math.floor(timeInTime / 100);
+	const timeInMinutes = Number.parseInt(timeInTime.toString().slice(-2)) / 60;
+	const timeOutHour = Math.floor(timeOutTime / 100);
+	const timeOutMinutes = Number.parseInt(timeOutTime.toString().slice(-2)) / 60;
+	debugger
+	return (timeOutHour + timeOutMinutes) - (timeInHour + timeInMinutes)
+}
+function hoursWorkedOnDate(dateWorked) {
+	const timeInTime = this.timeInEvents.find(event => event.date === dateWorked).hour;
+	const timeOutTime = this.timeOutEvents.find(event => event.date === dateWorked).hour;
+	if (!timeInTime || !timeOutTime) throw new Error("Missing punch detected");
+	return getDuration(timeInTime, timeOutTime)
+}
+
+function wagesEarnedOnDate(dateWorked) { return this.payPerHour * hoursWorkedOnDate.call(this, dateWorked) }
+
+function findEmployeeByFirstName(collection, firstNameString) {
+	return collection.find(record => record.firstName === firstNameString)
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -10,14 +67,16 @@
  */
 
 const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+	const eligibleDates = this.timeInEvents.map(function (e) {
+		return e.date
+	})
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+	const payable = eligibleDates.reduce(function (memo, d) {
+		return memo + wagesEarnedOnDate.call(this, d)
+	}.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+	return payable
 }
 
+
+function calculatePayroll() { return this.reduce((total, record) => total + allWagesFor(record), 0) }
